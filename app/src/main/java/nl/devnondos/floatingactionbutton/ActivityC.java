8fe6d704e3681e679c6249bcd8d64e6f357bec69
Victor Perez <viperey@gmail.com>
2014-10-25 17:48:17 +0200

    First commit and first stable version

---
+++ app/src/main/java/nl/devnondos/floatingactionbutton/ActivityC.java
@@ -0,0 +1,14 @@
+package nl.devnondos.floatingactionbutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ActivityC extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+    }
+}
